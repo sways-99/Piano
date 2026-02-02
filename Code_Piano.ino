bee93d8ae47ad108137051f03025214ca1b80432
sways-99 <christianps0518@gmail.com>
2026-02-02 13:47:49 -0600

    Update Code_Piano.ino

--- Code_Piano.ino
+++ Code_Piano.ino
@@ -1,8 +1,8 @@
 int buzzer = 9;
-int bt1 = 2;
-int bt2 =4;
-int bt3 = 5;
-int bt4 = 6;
+int bt1 = 2; //button1
+int bt2 =4; //button 2
+int bt3 = 5; //button 3
+int bt4 = 6; // button 4
 
 void setup(){
   
@@ -19,29 +19,29 @@ void loop(){
 }
 
 void sound(){
-  
-  if(digitalRead(bt1) == LOW && digitalRead(bt2) == LOW){
+
+  if(digitalRead(bt1) == LOW && digitalRead(bt2) == LOW){ // Sol
     tone(buzzer, 382);
-  }
-  else if(digitalRead(bt2) == LOW && digitalRead(bt3) == LOW){
+  }    
+  else if(digitalRead(bt2) == LOW && digitalRead(bt3) == LOW){ //La
     tone(buzzer, 440);
   } 
-  else if(digitalRead(bt3) == LOW && digitalRead(bt4) == LOW){
+  else if(digitalRead(bt3) == LOW && digitalRead(bt4) == LOW){ //Si
     tone(buzzer, 500);
   }
-  else if(digitalRead(bt1) == LOW && digitalRead(bt4) == LOW){
+  else if(digitalRead(bt1) == LOW && digitalRead(bt4) == LOW){ //DO 
     tone(buzzer, 532);
   } 
-  else if(digitalRead(bt1) == LOW){
+  else if(digitalRead(bt1) == LOW){ //DO
     tone(buzzer, 260);
   }
-  else if(digitalRead(bt2) == LOW){
+  else if(digitalRead(bt2) == LOW){ //Re
     tone(buzzer, 294);
   }
-  else if(digitalRead(bt3) == LOW){
+  else if(digitalRead(bt3) == LOW){ //MI 
     tone(buzzer, 330);
   }
-  else if(digitalRead(bt4) == LOW){
+  else if(digitalRead(bt4) == LOW){ //Fa
     tone(buzzer, 350);
   }
   else{
@@ -52,4 +52,4 @@ void sound(){
   }
 
 
-}
+
